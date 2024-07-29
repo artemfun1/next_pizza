a0e9cdc5afa30d46f5e8cd93d555dcd231509f35
artemfun1 <a.o.peshkov@yandex.ru>
2024-07-29 14:26:33 +0500

    creacte progect

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+  subsets: ['cyrillic'],
+  variable: '--font-nunito',
+  weight: ['400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+	title: "Next Pizza | Главная",
+	description: "This's my student project",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="ru">
+			<body className={nunito.className}>
+        
+        <main className='min-h-screen'>
+{children}
+
+        </main>
+        
+        
+
+
+
+      </body>
+		</html>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@/components/ui/button'
+
+
+
+
+
+
+
+
+
+export default function Home() {
+
+  console.log('mmmmmm')
+
+  return (
+    <>
+      <Button variant={'outline'}>Корзина</Button>
+      <h1>Главная</h1>
+    </>
+
+
+
+
+  );
+}
+
+
+
+
+
+
+
