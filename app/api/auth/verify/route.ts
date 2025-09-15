40e7ae25738259a78be6158eb539711c7ae523c1
artemfun1 <a.o.peshkov@yandex.ru>
2025-09-15 19:12:34 +0500

    add comment runtime mode

--- app/api/auth/verify/route.ts
+++ app/api/auth/verify/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 
 export const dynamic = 'force-dynamic';
-export const runtime = 'nodejs';
+// export const runtime = 'nodejs';
 
 export async function GET(req: NextRequest) {
   try {
